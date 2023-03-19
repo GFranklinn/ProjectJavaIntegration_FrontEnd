1fb952aa9a96ac5277489ddd3da556922e84167e
Gabriel Franklin <gabrielfranklin567@gmail.com>
2023-03-19 01:43:37 -0300

    Added front-end files

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("view/GridPaneView.fxml"));
+		Scene scene = new Scene(loader.load());
+		primaryStage.setTitle("Projeto Integração");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
