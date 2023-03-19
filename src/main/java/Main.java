6f56508c55490b392282fe5a7b6fa7b6021cf8e7
Gabriel Franklin <gabrielfranklin567@gmail.com>
2023-03-19 02:14:56 -0300

    removed import unused and new release

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
