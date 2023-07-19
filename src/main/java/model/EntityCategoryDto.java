ef6d446b5f372096388b3f4dfac4c4efda5fbee0
Gabriel Franklin <gabrielfranklin567@gmail.com>
2023-07-19 08:57:19 -0300

    added method equals

--- src/main/java/model/EntityCategoryDto.java
+++ src/main/java/model/EntityCategoryDto.java
@@ -6,9 +6,7 @@ public class EntityCategoryDto extends ObjectAbstractDto {
 
     private EntityLineDto entityLineDto;
 
-    public EntityCategoryDto() {
-    }
-
+    public EntityCategoryDto() {}
     public EntityCategoryDto(int id, String name, EntityLineDto entityLineDto) {
         super(name, id);
         this.entityLineDto = entityLineDto;
@@ -23,25 +21,12 @@ public class EntityCategoryDto extends ObjectAbstractDto {
     }
 
     @Override
-    public boolean equals(Object object) {
-        if (this == object) {
-            return true;
-        }
-
-        if (!(object instanceof EntityCategoryDto)) {
+    public boolean equals(Object obj) {
+        if (!(obj instanceof EntityCategoryDto)) {
             return false;
         }
 
-        if (!super.equals(object)) {
-            return false;
-        }
-
-        EntityCategoryDto differentCategory = (EntityCategoryDto) object;
-
-        return Objects.equals(entityLineDto, differentCategory.entityLineDto);
-    }
-    @Override
-    public String toString() {
-        return getName();
+        EntityCategoryDto otherCategory = (EntityCategoryDto) obj;
+        return super.equals(obj) && entityLineDto.equals(otherCategory.entityLineDto);
     }
 }
